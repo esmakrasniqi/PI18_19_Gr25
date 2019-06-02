68c68a004e0e9eca89c5ca4d225c6e81b34a9a52
Esma Krasniqi <44786311+esmakrasniqi@users.noreply.github.com>
2019-06-02 12:59:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,76 +1,185 @@
-<?php
-session_start();
-include("dbconnection.php");
+<?php session_start();
+require_once('dbconnection.php');
+
+//Code for Registration 
+if(isset($_POST['signup']))
+{
+	$fname=$_POST['fname'];
+	$lname=$_POST['lname'];
+	$email=$_POST['email'];
+	$password=$_POST['password'];
+	$contact=$_POST['contact'];
+	$enc_password=md5($password);
+	$a=date('Y-m-d');
+	$msg=mysqli_query($con,"insert into users(fname,lname,email,password,contactno,posting_date) values('$fname','$lname','$email','$enc_password','$contact','$a')");
+if($msg)
+{
+	echo "<script>alert('Register successfully');</script>";
+}
+}
+
+// Code for login system
 if(isset($_POST['login']))
 {
-  $adminusername=$_POST['username'];
-  $pass=md5($_POST['password']);
-$ret=mysqli_query($con,"SELECT * FROM admin WHERE username='$adminusername' and password='$pass'");
+$password=$_POST['password'];
+$dec_password=md5($password);
+$useremail=$_POST['uemail'];
+$ret= mysqli_query($con,"SELECT * FROM users WHERE email='$useremail' and password='$dec_password'");
 $num=mysqli_fetch_array($ret);
 if($num>0)
 {
-$extra="manage-users.php";
-$_SESSION['login']=$_POST['username'];
+$extra="welcome.php";
+$_SESSION['login']=$_POST['uemail'];
 $_SESSION['id']=$num['id'];
-echo "<script>window.location.href='".$extra."'</script>";
+$_SESSION['name']=$num['fname'];
+$host=$_SERVER['HTTP_HOST'];
+$uri=rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+header("location:http://$host$uri/$extra");
 exit();
 }
 else
 {
-$_SESSION['action1']="*Invalid username or password";
+echo "<script>alert('Invalid username or password');</script>";
 $extra="index.php";
-echo "<script>window.location.href='".$extra."'</script>";
+$host  = $_SERVER['HTTP_HOST'];
+$uri  = rtrim(dirname($_SERVER['PHP_SELF']),'/\\');
+//header("location:http://$host$uri/$extra");
 exit();
 }
 }
 
+//Code for Forgot Password
+
+if(isset($_POST['send']))
+{
+$row1=mysqli_query($con,"select email,password from users where email='".$_POST['femail']."'");
+$row2=mysqli_fetch_array($row1);
+if($row2>0)
+{
+$email = $row2['email'];
+$subject = "Information about your password";
+$password=$row2['password'];
+$message = "Your password is ".$password;
+mail($email, $subject, $message, "From: $email");
+echo  "<script>alert('Your Password has been sent Successfully');</script>";
+}
+else
+{
+echo "<script>alert('Email not register with us');</script>";	
+}
+}
+
 ?>
-
-
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="Dashboard">
-    <meta name="keyword" content="Dashboard, Bootstrap, Admin, Template, Theme, Responsive, Fluid, Retina">
+<html>
+<head>
+<title>Login System</title>
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Elegent Tab Forms,Login Forms,Sign up Forms,Registration Forms,News latter Forms,Elements"./>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+</script>
+<script src="js/jquery.min.js"></script>
+<script src="js/easyResponsiveTabs.js" type="text/javascript"></script>
+				<script type="text/javascript">
+					$(document).ready(function () {
+						$('#horizontalTab').easyResponsiveTabs({
+							type: 'default',       
+							width: 'auto', 
+							fit: true 
+						});
+					});
+				   </script>
+<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,400,600,700,200italic,300italic,400italic,600italic|Lora:400,700,400italic,700italic|Raleway:400,500,300,600,700,200,100' rel='stylesheet' type='text/css'>
+</head>
+<body>
+<div class="main">
+		<h1>Sistemi per hyrje dhe regjistrim</h1>
+	 <div class="sap_tabs">	
+			<div id="horizontalTab" style="display: block; width: 100%; margin: 0px;">
+			  <ul class="resp-tabs-list">
+			  	  <li class="resp-tab-item" aria-controls="tab_item-0" role="tab"><div class="top-img"><img src="images/top-note.png" alt=""/></div><span>Regjistrohu</span>
+			  	  	
+				</li>
+				  <li class="resp-tab-item" aria-controls="tab_item-1" role="tab"><div class="top-img"><img src="images/top-lock.png" alt=""/></div><span>Hyr</span></li>
+				  <li class="resp-tab-item lost" aria-controls="tab_item-2" role="tab"><div class="top-img"><img src="images/top-key.png" alt=""/></div><span>Keni harruar fjalekalimin?</span></li>
+				  <div class="clear"></div>
+			  </ul>		
+			  	 
+			<div class="resp-tabs-container">
+					<div class="tab-1 resp-tab-content" aria-labelledby="tab_item-0">
+					<div class="facts">
+					
+						<div class="register">
+							<form name="registration" method="post" action="" enctype="multipart/form-data">
+								<p>Emri </p>
+								<input type="text" class="text" value=""  name="fname" required >
+								<p>Mbiemri </p>
+								<input type="text" class="text" value="" name="lname"  required >
+								<p>Email Adresa</p>
+								<input type="text" class="text" value="" name="email"  >
+								<p>Fjalekalimi </p>
+								<input type="password" value="" name="password" required>
+										<p>Numri i telefonit</p>
+								<input type="text" value="" name="contact"  required>
+								<div class="sign-up">
+									<input type="reset" value="Reseto">
+									<input type="submit" name="signup"  value="Regjistrohu" >
+									<div class="clear"> </div>
+								</div>
+							</form>
 
-    <title>Admin | Login</title>
-    <link href="assets/css/bootstrap.css" rel="stylesheet">
-    <link href="assets/font-awesome/css/font-awesome.css" rel="stylesheet" />
-    <link href="assets/css/style.css" rel="stylesheet">
-    <link href="assets/css/style-responsive.css" rel="stylesheet">
-  </head>
+						</div>
+					</div>
+				</div>		
+			 <div class="tab-2 resp-tab-content" aria-labelledby="tab_item-1">
+					 	<div class="facts">
+							 <div class="login">
+							<div class="buttons">
+								
+								
+							</div>
+							<form name="login" action="" method="post">
+								<input type="text" class="text" name="uemail" value="" placeholder="Shkruani email adresen"  ><a href="#" class=" icon email"></a>
 
-  <body>
-	  <div id="login-page">
-	  	<div class="container">
-      
-	  	
-		      <form class="form-login" action="" method="post">
-		        <h2 class="form-login-heading">Lidhu</h2>
-                  <p style="color:#F00; padding-top:20px;" align="center"><?php echo $_SESSION['action1'];?><?php echo $_SESSION['action1']="";?></p>
-		        <div class="login-wrap">
-		            <input type="text" name="username" class="form-control" placeholder="ID e perdoruesit" autofocus>
-		            <br>
-		            <input type="password" name="password" class="form-control" placeholder="Fjalekalimi"><br >
-		            <input  name="login" class="btn btn-theme btn-block" type="submit">
-		         
+								<input type="password" value="" name="password" placeholder="Shkruani fjalekalimin"><a href="#" class=" icon lock"></a>
+
+								<div class="p-container">
+								
+									<div class="submit two">
+									<input type="submit" name="login" value="Hyr" >
+									</div>
+									<div class="clear"> </div>
+								</div>
+
+							</form>
+					</div>
+				</div> 
+			</div> 			        					 
+				 <div class="tab-2 resp-tab-content" aria-labelledby="tab_item-1">
+					 	<div class="facts">
+							 <div class="login">
+							<div class="buttons">
+								
+								
+							</div>
+							<form name="login" action="" method="post">
+								<input type="text" class="text" name="femail" value="" placeholder="Shkruani email adresen" required  ><a href="#" class=" icon email"></a>
+									
+										<div class="submit three">
+											<input type="submit" name="send" onClick="myFunction()" value="Dergo email" >
+										</div>
+									</form>
+									</div>
+				         	</div>           	      
+				        </div>	
+				     </div>	
 		        </div>
-		      </form>	  	
-	  	
-	  	</div>
-	  </div>
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.backstretch.min.js"></script>
-    <script>
-        $.backstretch("assets/img/login-bg.jpg", {speed: 500});
-    </script>
-
-<footer>
+	        </div>
+	     </div>
+		 <footer>
 <p style="text-align:center"> Copyright &copy; FIEK 2019 Grupi 25 - All rights reserved. </p>
 </footer>
-  </body>
+</body>
 </html>
